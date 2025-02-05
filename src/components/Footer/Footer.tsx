83b4b6ef8b970d83cf3b1f7fdfb2caea0fd33f4b
Tisma <lb.brangeon@gmail.com>
2025-02-05 11:05:44 +0100

    🐛 Fix merge className for footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,18 +1,28 @@
-import type { JSX, ReactNode } from "react";
+import type { JSX } from "react";
+import { twMerge } from "tailwind-merge";
 
 /* ROOT */
-type RootProps = { children: ReactNode };
-const Root = ({ children }: RootProps) => (
-	<div className="absolute bottom-4 left-1/2 flex max-w-[90vw] -translate-x-1/2 flex-row items-center justify-center gap-2 rounded-xl bg-white p-4 shadow-lg">
+type RootProps = JSX.IntrinsicElements["div"];
+const Root = ({ children, ...props }: RootProps) => (
+	<div
+		{...props}
+		className={twMerge(
+			"absolute bottom-4 left-1/2 flex max-w-[90vw] -translate-x-1/2 flex-row items-center justify-center gap-2 rounded-xl bg-white p-4 shadow-lg",
+			props.className,
+		)}
+	>
 		{children}
 	</div>
 );
 Root.displayName = "FooterRoot";
 
 /* ITEM */
-type ItemProps = Omit<JSX.IntrinsicElements["button"], "className">;
+type ItemProps = JSX.IntrinsicElements["button"];
 const Item = ({ children, ...props }: ItemProps) => (
-	<button {...props} className="rounded-lg bg-gray-200 px-4 py-2">
+	<button
+		{...props}
+		className={twMerge("rounded-lg bg-gray-200 px-4 py-2", props.className)}
+	>
 		{children}
 	</button>
 );
